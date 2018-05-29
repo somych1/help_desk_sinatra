a00920a7dca0b4c019585f135ecf279c535ec542
Samat Baltin <somych1@gmail.com>
2018-05-29 15:05:59 -0500

    created driver login/register routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Driver from './Driver'
 import Employee from './Employee'
 
 class App extends Component {
@@ -56,10 +56,70 @@ class App extends Component {
       })
     }
   }
+  logout = async () => {
+    const empLogout = await fetch('http://localhost:9292/emp/logout', {
+        method: 'GET'
+    });
+    const logoutResponse = await empLogout.json();
+    this.setState({
+      loggedIn: false
+    })
+  }
+
+  // driver register
+  driverRegister = async (name, username, password, truck, make, model, year) => {
+    const driverRegister = await fetch('http://localhost:9292/driver/register', {
+      method: 'POST',
+      credentials: 'include',
+      body: JSON.stringify({
+        name: name,
+        username: username,
+        password: password,
+        truck_num: truck,
+        make: make,
+        model: model,
+        yearL: year
+      })
+    })
+    const registrationResponse = await driverRegister.json();
+    console.log(registrationResponse, 'this is registrationResponse')
+    if(registrationResponse.success){
+      this.setState({
+        loggedIn: true
+      })
+    } else {
+      this.setState({
+        loginError: registrationResponse.message
+      })
+    }
+  }
+  login = async (username, password) => {
+    const driverLogin = await fetch('http://localhost:9292/driver/login', {
+      method: 'POST',
+      credentials: 'include',
+      body: JSON.stringify({
+        username: username,
+        password: password
+      })
+    })
+    const loginResponse = await driverLogin.json()
+    console.log(loginResponse, 'this is loginResponse')
+    if(loginResponse.success){
+      this.setState({
+        loggedIn: true
+      })
+    } else {
+      this.setState({
+        loginError: loginResponse.message
+      })
+    }
+  }
   render() {
     return (
       <div className="App">
+
         <Employee register={this.register} login={this.login}/>
+        <Driver driverLogin={this.driverLogin} driverRegister={this.driverRegister}/>
       </div>
     );
   }
---
+++ src/Driver/index.js
@@ -0,0 +1,68 @@
+import React, {Component} from "react";
+import './style.css'
+
+class Driver extends Component{
+	constructor(){
+		super();
+		this.state = {
+			name: '',
+			username: '',
+			password: '',
+			truck: '',
+			make: '',
+			model: '',
+			year: '',
+			registering: false
+		}
+	}
+	registration = () => {
+		this.setState({
+			registering: true
+		})
+	}
+	login = () => {
+		this.setState({
+			registering: false
+		})
+	}
+	handleInput = (e) => {
+		const field = e.currentTarget.name
+		if(field == 'name') this.setState({ name: e.currentTarget.value })
+		else if (field == 'username') this.setState({ username: e.currentTarget.value })
+		else if (field == 'password') this.setState({ password: e.currentTarget.value });
+		else if (field == 'truck') this.setState({ truck: e.currentTarget.value });
+		else if (field == 'make') this.setState({ make: e.currentTarget.value });
+		else if (field == 'model') this.setState({ model: e.currentTarget.value });
+		else if (field == 'year') this.setState({ year: e.currentTarget.value });
+
+		// console.log(this.state.manager, 'this is manager')
+	}
+
+	handleSubmit = (e) => {
+		e.preventDefault();
+		if(this.state.registering) this.props.register(this.state.name, this.state.username, this.state.password, this.state.truck, this.state.make, this.state.model, this.state.year)
+		else this.props.login(this.state.username, this.state.password)
+	}
+	render(){
+		console.log(this.state.manager, 'this is manager')
+		return(
+			<div>
+				<button className={this.state.registering ? "current" : null} className="button" onClick={this.registration}>Create new user</button>
+				<button className={this.state.registering ? null : "current"} className="button" onClick={this.login}>Login</button>
+				<form onSubmit={this.handleSubmit}>
+					<input className={this.state.registering ? null : 'hide'} type='text' name='name' placeholder='name' value={this.state.name} onChange={this.handleInput}/><br />
+					<input type='text' name='username' placeholder='username' value={this.state.username} onChange={this.handleInput}/><br />
+					<input type='password' name='password' placeholder='password' value={this.state.password} onChange={this.handleInput}/><br />
+					<input className={this.state.registering ? null : 'hide'}  type='text' name='truck' placeholder='truck' value={this.state.truck} onChange={this.handleInput}/><br />
+					<input className={this.state.registering ? null : 'hide'}  type='text' name='make' placeholder='make' value={this.state.make} onChange={this.handleInput}/><br />
+					<input className={this.state.registering ? null : 'hide'}  type='text' name='model' placeholder='model' value={this.state.model} onChange={this.handleInput}/><br />
+					<input className={this.state.registering ? null : 'hide'}  type='text' name='year' placeholder='year' value={this.state.year} onChange={this.handleInput}/><br />
+					<button className="button button-primary" type='submit'>Submit</button>
+				</form>
+				{this.props.loginError != '' ? <p>{this.props.loginError}</p> : null}
+			</div>
+		)
+	}
+}
+
+export default Driver
